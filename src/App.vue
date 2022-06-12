20593d30676d049356c57f123c3dbf206c2c5bb0
xeave <2859397904@qq.com>
2022-06-13 00:45:11 +0800

    底部组件引用

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <template>
   <div id="app">
     <my-header>星座物语</my-header>
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <keep-alive >
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+    <tab />
   </div>
 </template>
 
 <script>
 import MyHeader from "@/components/Header";
+import Tab from "@/components/Tab";
+
+import { watch } from "vue";
+
+import { useStore } from "vuex";
+
+import { useRouter } from "vue-router";
+
 export default {
   name: "App",
-  components: { MyHeader },
+  components: { MyHeader, Tab },
+  setup() {
+    const store = useStore(),
+      router = useRouter();
+    router.push("/");
+    store.commit("setField", "today");
+    watch(
+      () => {
+        return router.currentRoute.value.name;
+      },
+      (value) => {
+        store.commit("setField", value);
+      }
+    );
+  },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const {
   defineConfig
 } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
+  // transpileDependencies: true,
+  lintOnSave: false,
   devServer: {
     proxy: {
       '/api': {
